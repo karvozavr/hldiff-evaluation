f5258731cbf22df3d7985895a882e8607a4c7351
karvozavr <karvozavr@gmail.com>
2020-04-25 13:20:05 +0300

    Fix

--- processor.py
+++ processor.py
@@ -1,4 +1,5 @@
 import os
+import random
 from shutil import copyfile
 import logging
 
@@ -25,16 +26,19 @@ def save(repo, commit, filename, before, after):
 
 class RepositoryProcessor:
 
-    def __init__(self, repository: str):
+    def __init__(self, repository: str, owner: str):
+        self.owner = owner
         self.repository = os.path.split(repository)[-1]
         self.repo = GitRepository(repository)
         self.mining = RepositoryMining(repository)
         self.pairs = []
+        random.seed(42)
 
     def run(self):
         self.get_all_filepairs()
         with open(os.path.join('filepairs', self.repository, 'pairs.txt'), 'w') as f:
-            f.write('\n'.join(map(lambda x: f'{x[0]} {x[1]}', self.pairs)))
+            f.write('\n'.join(map(lambda x: f'{x[0]} {x[1]} {x[2]}', self.pairs)))
+        f.write('\n')
 
     def get_all_filepairs(self, file_filter=java_file_filter):
         commits = list(filter(lambda x: not x.merge, self.mining.traverse_commits()))
@@ -64,14 +68,36 @@ class RepositoryProcessor:
         after_saved = os.path.join(path, 'after__' + commit_hash + '_' + filename)
         copyfile(after, after_saved)
 
-        self.pairs.append((before_saved, after_saved))
+        self.pairs.append((before_saved, after_saved, commit_hash + '.' + self.owner + '.' + before.replace('/', '.')))
+
+    def run_random(self, number):
+        self.get_random_filepairs(number)
+        with open(os.path.join('filepairs', self.repository, 'pairs.txt'), 'w') as f:
+            f.write('\n'.join(map(lambda x: f'{x[0]} {x[1]} {x[2]}', self.pairs)))
+            f.write('\n')
+
+    def get_random_filepairs(self, number, file_filter=java_file_filter):
+        commits = random.choices(list(filter(lambda x: not x.merge, self.mining.traverse_commits())), k=number)
+        for idx, commit in enumerate(commits):
+            print(f'Processing commit №{idx}: {commit.hash}')
+            for modification in commit.modifications:
+                if modification.change_type == ModificationType.MODIFY:
+                    if file_filter(modification.filename):
+                        self.get_file_pair(commit, modification)
 
 
 @click.command()
 @click.argument('repository')
-def filepairs(repository):
-    processor = RepositoryProcessor(repository)
-    processor.run()
+@click.option('-n', '--nrandom')
+@click.option('--owner')
+def filepairs(repository, nrandom, owner):
+    processor = RepositoryProcessor(repository, owner)
+    nrandom = int(nrandom)
+    if nrandom is not None:
+        print(f'Random {nrandom}')
+        processor.run_random(nrandom)
+    else:
+        processor.run()
 
 
 if __name__ == '__main__':
